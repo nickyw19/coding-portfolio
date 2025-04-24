7060e12e0c390a47af0a28b8f1344022d9b3b212
Nicky W <nirat.nicky@gmail.com>
2025-04-24 10:16:38 +0700

    Added files for Coding Portfolio project

---
+++ app.py
@@ -0,0 +1,10 @@
+"""Create a Flask App Instance to create a web app """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+app.run(debug=True)
